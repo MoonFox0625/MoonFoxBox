7f4b8a61174815b9043c31935b96a12cb987fad3
Wang Sen <137757130@qq.com>
2021-09-28 23:36:42 +0800

    router requests

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# moonfoxBox
+# MoonFoxBox
 
 这是一个个人学习项目：
 
 golang版本： go1.16
 
+recent Router
+
+| URL Pattern     | Handler       | Action                     |
+| --------------- | ------------- | -------------------------- |
+| /               | home          | Display the hoem page      |
+| /snippet        | showSnippet   | Display a specific snippet |
+| /snippet/create | createSnippet | Create a new snippet       |
+
 2021-9-28 16:43:41 createProject
 
-
+2021-9-28 23:53:08 chapter2.4 router requests
 
--- main.go
+++ main.go
@@ -7,13 +7,32 @@ import (
 	"net/http"
 )
 
+// home : Displaying the home page
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	_, _ = w.Write([]byte("Hello MoonFoxBox"))
 }
+
+// showSnippet : Display a specific snippet
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// createSnippet:Create a new snippet
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
+// main function
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Staring server on 4000")
 	err := http.ListenAndServe(":4000", mux)
