4fd8e423e5da0bb2f88457864ada25093e8da856
Wang Sen <137757130@qq.com>
2021-10-04 22:06:44 +0800

    Executing SQL Statements

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -6,7 +6,6 @@ package mysql
 import (
 	"MoonFoxBox/pkg/models"
 	"database/sql"
-	"time"
 )
 
 // SnippetModel Define a SnippetModel type which wraps a sql.DB connection pool.
@@ -15,8 +14,23 @@ type SnippetModel struct {
 }
 
 // Insert This will insert a new snippet into the database.
-func (sm *SnippetModel) Insert(title, content string, expires time.Time) (int, error) {
-	return 0, nil
+func (sm *SnippetModel) Insert(title, content, expires string) (int, error) {
+	// Write the SQL statement we want to execute. I've split it over two lines
+	// for readability (which is why it's surrounded with backquotes instead
+	// of normal double quotes).
+	stmt := `INSERT INTO snippets(title, content, created, expires)
+VALUES (?, ?, UTC_TIMESTAMP,DATE_ADD(UTC_TIMESTAMP,INTERVAL ? DAY ))`
+
+	result, err := sm.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+	return int(id), nil
 }
 
 // Get This will return a specific snippet based on its id.
