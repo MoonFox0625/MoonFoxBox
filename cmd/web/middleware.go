318522839710c75406017eb4fafda0c80464045b
Wang Sen <137757130@qq.com>
2021-10-08 22:30:05 +0800

    Panic Recovery

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,7 +3,10 @@
 // Desc:
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // Set Header X-Frame-Options: deny X-XSS-Protection: 1; mode=block
 func secureHeaders(next http.Handler) http.Handler {
@@ -21,4 +24,25 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event
+		// of a panic as Go unwinds the stack).
+		defer func() {
+			// Use the builtin recover function to check if there has been a
+			// panic or not. If there has...
+			if err := recover(); err != nil {
+				// Set a "Connection: close" header on the response.
+				w.Header().Set("Connection", "close")
+
+				app.serverError(w, fmt.Errorf("%s", err))
+				return
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+
 }
